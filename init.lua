7561e5bd11632d4bc237881ffba9babb2ad5287a
Marcus <ukendio@gmail.com>
2022-04-09 15:37:46 +0200

    refactor

---
+++ init.lua
@@ -0,0 +1,5 @@
+local SymbolicAlgebra = require(script.symbolicAlgebra)
+
+return {
+    symbolicAlgebra = SymbolicAlgebra
+}
---
+++ symbolicAlgebra/init.lua
@@ -0,0 +1,6 @@
+return {
+    set = require(script.Set),
+    duple = require(script.duple),
+    monomial = require(script.monomial),
+    polynomial = require(script.polynomial)
+}
